08ad6dcd11af8b95a1c01fad1210d25513533c14
Dipen <dipen@example.com>
2023-08-13 19:49:10 +0545

    UI Part Completed

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,21 @@ function App() {
         </div>
 
         <div className={classes.ProductData}>
-          <h1>{ProductData.title}</h1>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          <p className={classes.ProductDesc}>{ProductData.description}</p>
+          <h3 className={classes.SectionHeading} >Select Color :</h3>
+          <div>
+            <img className={[classes.ProductImg, classes.selectedimg].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black" />
+            <img className={classes.ProductImg} src="https://imgur.com/PTgQlim.png" alt="Red" />
+            <img className={classes.ProductImg} src="https://imgur.com/Mplj1YR.png" alt="Blue" />
+            <img className={classes.ProductImg} src="https://imgur.com/xSIK4M8.png" alt="Purple" />
+          </div>
+          <h3 className={classes.SectionHeading}>Features</h3>
+          <div>
+            <button className={[classes.featuresBtn, classes.selectedFeatureItem].join(' ')} >Time</button>
+            <button className={classes.featuresBtn} >HeartRate</button>
+          </div>
+          <button className={classes.primaryBtn} >Buy Now</button>
         </div>
 
       </div>
